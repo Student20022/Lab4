b2ddf0caa120aaf54157006bd390f7b08ae148a4
Student20022 <131999657+Student20022@users.noreply.github.com>
2023-10-27 12:58:07 +0300

    Add files via upload

---
+++ lab4_task1/lab4_task1/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+// Клас "пряма на площині"
+class TLine2D
+{
+    private double a; // Коефіцієнт 'a' канонічного рівняння
+    private double b; // Коефіцієнт 'b' канонічного рівняння
+    private double c; // Коефіцієнт 'c' канонічного рівняння
+
+    // Конструктор без параметрів
+    public TLine2D()
+    {
+        a = 0;
+        b = 0;
+        c = 0;
+    }
+
+    // Конструктор з параметрами
+    public TLine2D(double a, double b, double c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    // Конструктор копіювання
+    public TLine2D(TLine2D otherLine)
+    {
+        this.a = otherLine.a;
+        this.b = otherLine.b;
+        this.c = otherLine.c;
+    }
+
+    // Введення даних
+    public void Input()
+    {
+        Console.Write("Введіть коефіцієнт 'a': ");
+        a = double.Parse(Console.ReadLine());
+        Console.Write("Введіть коефіцієнт 'b': ");
+        b = double.Parse(Console.ReadLine());
+        Console.Write("Введіть коефіцієнт 'c': ");
+        c = double.Parse(Console.ReadLine());
+    }
+
+    // Виведення даних
+    public void Display()
+    {
+        Console.WriteLine($"Рівняння прямої: {a}x + {b}y + {c} = 0");
+    }
+
+    // Знаходження точки перетину з іншою прямою
+    public bool IntersectsWith(TLine2D otherLine, out double x, out double y)
+    {
+        x = 0;
+        y = 0;
+
+        double determinant = a * otherLine.b - otherLine.a * b;
+
+        if (determinant == 0)
+        {
+            // Прямі паралельні або співпадають
+            return false;
+        }
+        else
+        {
+            x = (otherLine.c * b - c * otherLine.b) / determinant;
+            y = (a * otherLine.c - otherLine.a * c) / determinant;
+            return true;
+        }
+    }
+
+    // Визначення належності точки прямій
+    public bool ContainsPoint(double x, double y)
+    {
+        return a * x + b * y + c == 0;
+    }
+
+    // Перевантаження операторів
+    public static TLine2D operator +(TLine2D line1, TLine2D line2)
+    {
+        return new TLine2D(line1.a + line2.a, line1.b + line2.b, line1.c + line2.c);
+    }
+
+    public static TLine2D operator -(TLine2D line1, TLine2D line2)
+    {
+        return new TLine2D(line1.a - line2.a, line1.b - line2.b, line1.c - line2.c);
+    }
+}
+
+// Клас-нащадок для прямої в просторі
+class TLine3D : TLine2D
+{
+    private double d; // Коефіцієнт 'd' канонічного рівняння
+
+    // Конструктор без параметрів
+    public TLine3D()
+    {
+        d = 0;
+    }
+
+    // Конструктор з параметрами
+    public TLine3D(double a, double b, double c, double d) : base(a, b, c)
+    {
+        this.d = d;
+    }
+
+    // Конструктор копіювання
+    public TLine3D(TLine3D otherLine) : base(otherLine)
+    {
+        this.d = otherLine.d;
+    }
+
+    // Введення даних
+    public new void Input()
+    {
+        base.Input(); // Виклик методу базового класу
+        Console.Write("Введіть коефіцієнт 'd': ");
+        d = double.Parse(Console.ReadLine());
+    }
+
+    // Виведення даних
+    public new void Display()
+    {
+        base.Display(); // Виклик методу базового класу
+        Console.WriteLine($"Коефіцієнт 'd': {d}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TLine2D line1 = new TLine2D(2, -3, 4);
+        TLine2D line2 = new TLine2D(1, 1, -5);
+
+        TLine2D line3 = line1 + line2;
+        TLine2D line4 = line1 - line2;
+
+        line1.Display();
+        line2.Display();
+        line3.Display();
+        line4.Display();
+
+        TLine3D line3D = new TLine3D(2, -3, 4, 1);
+        line3D.Display();
+    }
+}
---
+++ lab4_task2/lab4_task2/Program.cs
@@ -0,0 +1,160 @@
+﻿using System.Drawing;
+
+public class TSMatrix
+{
+    public int[,] matrix;
+    public int size;
+
+    public TSMatrix()
+    {
+        size = 0;
+        matrix = new int[size, size];
+    }
+
+    public TSMatrix(int size)
+    {
+        this.size = size;
+        matrix = new int[size, size];
+    }
+
+    public TSMatrix(TSMatrix other)
+    {
+        size = other.size;
+        matrix = other.matrix;
+    }
+
+    public void InputData()
+    {
+        // Введення даних
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                matrix[i, j] = int.Parse(Console.ReadLine());
+    }
+
+    public void OutputData()
+    {
+        // Виведення даних
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+                Console.Write(matrix[i, j] + " ");
+            Console.WriteLine();
+        }
+    }
+
+    public int FindMax()
+    {
+        // Знаходження максимального елемента
+        int max = matrix[0, 0];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                if (matrix[i, j] > max)
+                    max = matrix[i, j];
+        return max;
+    }
+
+    public int FindMin()
+    {
+        // Знаходження мінімального елемента
+        int min = matrix[0, 0];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                if (matrix[i, j] < min)
+                    min = matrix[i, j];
+        return min;
+    }
+
+    public int SumElements()
+    {
+        // Знаходження суми елементів
+        int sum = 0;
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                sum += matrix[i, j];
+        return sum;
+    }
+
+    public static TSMatrix operator +(TSMatrix a, TSMatrix b)
+    {
+        // Додавання матриць
+        TSMatrix result = new TSMatrix(a.size);
+        for (int i = 0; i < a.size; i++)
+            for (int j = 0; j < a.size; j++)
+                result.matrix[i, j] = a.matrix[i, j] + b.matrix[i, j];
+        return result;
+    }
+
+    public static TSMatrix operator -(TSMatrix a, TSMatrix b)
+    {
+        // Віднімання матриць
+        TSMatrix result = new TSMatrix(a.size);
+        for (int i = 0; i < a.size; i++)
+            for (int j = 0; j < a.size; j++)
+                result.matrix[i, j] = a.matrix[i, j] - b.matrix[i, j];
+        return result;
+    }
+}
+
+public class TMSMatrix : TSMatrix
+{
+    public TMSMatrix() : base() { }
+
+    public TMSMatrix(int size) : base(size) { }
+
+    public TMSMatrix(TSMatrix other) : base(other) { }
+
+    public void Transpose()
+    {
+        // Транспонування матриці
+        for (int i = 0; i < size - 1; i++)
+            for (int j = i + 1; j < size; j++)
+            {
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[j, i];
+                matrix[j, i] = temp;
+            }
+    }
+
+    public static TMSMatrix operator *(TMSMatrix a, TMSMatrix b)
+    {
+        // Множення матриць
+        TMSMatrix result = new TMSMatrix(a.size);
+        for (int i = 0; i < a.size; i++)
+            for (int k = 0; k < a.size; k++)
+                for (int j = 0; j < a.size; j++)
+                    result.matrix[i, k] += a.matrix[i, k] * b.matrix[k, k];
+        return result;
+    }
+
+    public static TMSMatrix operator *(TMSMatrix a, int number)
+    {
+        // Множення матриці на число
+        TMSMatrix result = new TMSMatrix(a.size);
+        for (int i = 0; i < a.size; i++)
+            for (int j = 0; j < a.size; j++)
+                result.matrix[i, j] = a.matrix[i, j] * number;
+        return result;
+    }
+
+    public int FindDeterminant()
+    {
+        // Знаходження визначника
+        int det = 1;
+        for (int i = 0; i < size; i++)
+            det *= matrix[i, i];
+        return det;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        TSMatrix matrix1 = new TSMatrix(3);
+        matrix1.InputData();
+        matrix1.OutputData();
+
+        TMSMatrix matrix2 = new TMSMatrix(3);
+        matrix2.InputData();
+        matrix2.OutputData();
+    }
+}
